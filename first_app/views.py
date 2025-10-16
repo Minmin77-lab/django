05276acd59e1a22856f47499233f54e56cc3bb2e
Студент группы IS <is@sielom.local>
2025-10-16 09:47:45 +0500

    16.10

--- first_app/views.py
+++ first_app/views.py
@@ -2,11 +2,4 @@ from django.shortcuts import render, HttpResponse
 from .models import Users
 
 def root(request):
-    test = request.GET['test'] 
-    # if request.method == 'POST':
-    #     a = Users()
-    #     Users.save()
-    # elif request.method == 'GET':
-    #     return render ('', {'users':Users.objects.all()})
-    
-    # return render(request, 'one.html', {'my_note': text})
+    return render(request, 'main.catalog.html')
