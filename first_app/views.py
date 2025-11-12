6bb2840da1417698e30168b534522c1d11510657
Студент группы IS <is@sielom.local>
2025-11-12 09:26:48 +0500

    12.11 поиск и фильтрация

--- first_app/views.py
+++ first_app/views.py
@@ -1,26 +1,85 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
 from .models import Users, Staff, Attractions, Tickets, TicketTypes
 
 def attractions(request):
+    query = request.GET.get('query', '').strip()
     all_attractions = Attractions.objects.select_related('staff').all()
-    return render(request, 'attractions.html', {'attractions_list': all_attractions})
+    
+    if query:
+        all_attractions = all_attractions.filter(
+            Q(name__icontains=query) |
+            Q(staff__name__icontains=query) |
+            Q(staff__surname__icontains=query)
+        )
+    
+    return render(request, 'attractions.html', {
+        'attractions_list': all_attractions,
+        'search_query': query
+    })
 
 def tickets(request):
+    query = request.GET.get('query', '').strip()
+    selected_types = request.GET.getlist('type')
+    
     all_tickets = Tickets.objects.select_related('user', 'ticket_type').all().order_by('-purchase_date')
     all_ticket_types = TicketTypes.objects.all()
+    
+    # Применяем фильтрацию по типам
+    if selected_types:
+        all_tickets = all_tickets.filter(ticket_type__name__in=selected_types)
+    
+    # Применяем поиск
+    if query:
+        all_tickets = all_tickets.filter(
+            Q(user__name__icontains=query) |
+            Q(user__surname__icontains=query) |
+            Q(ticket_type__name__icontains=query) |
+            Q(id__icontains=query)
+        )
+    
     context = {
         'tickets_list': all_tickets,
         'ticket_types': all_ticket_types,
+        'selected_types': selected_types,
+        'search_query': query
     }
+    
     return render(request, 'tickets.html', context)
 
 def users(request):
-    all_users = Users.objects.all().order_by('surname', 'name') 
-    return render(request, 'users.html', {'users_list': all_users})
+    query = request.GET.get('query', '').strip()
+    all_users = Users.objects.all().order_by('surname', 'name')
+    
+    if query:
+        all_users = all_users.filter(
+            Q(name__icontains=query) |
+            Q(surname__icontains=query) |
+            Q(email__icontains=query) |
+            Q(phone_number__icontains=query)
+        )
+    
+    return render(request, 'users.html', {
+        'users_list': all_users,
+        'search_query': query
+    })
 
 def staff(request):
-    all_staff = Staff.objects.all().order_by('surname', 'name')  
-    return render(request, 'staff.html', {'staff_list': all_staff})
+    query = request.GET.get('query', '').strip()
+    all_staff = Staff.objects.all().order_by('surname', 'name')
+    
+    if query:
+        all_staff = all_staff.filter(
+            Q(name__icontains=query) |
+            Q(surname__icontains=query) |
+            Q(position__icontains=query) |
+            Q(phone_number__icontains=query)
+        )
+    
+    return render(request, 'staff.html', {
+        'staff_list': all_staff,
+        'search_query': query
+    })
 
 def attraction_detail(request, pk):
     attraction_data = get_object_or_404(Attractions, pk=pk)
